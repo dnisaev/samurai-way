504edd74d225e6d8d8eb18286fa8732d256e40f5
dnisaev <dnisaev@yandex.ru>
2023-11-22 23:47:43 +0300

    add Post

---
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from "./Post.module.css";
+
+const Post = () => {
+    return (
+        <div className={s.post}>Post 1</div>
+    );
+};
+
+export default Post;
