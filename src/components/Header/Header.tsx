328772c8640157eb60b5dbd8c4ce7b2f76e37051
dnisaev <dnisaev@yandex.ru>
2023-11-22 23:44:23 +0300

    update Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from "./Header.module.css";
 
 const Header = () => {
     return (
-        <header className={'header'}>
+        <header className={s.header}>
             <img src={"https://img.artlebedev.ru/everything/russia-logo/russia-logo.gif"} alt={"logo"}
                  width={"100px"}/>
         </header>
