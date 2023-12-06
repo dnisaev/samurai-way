514b5cf4a937e32db79101a5479583ae3e7fcac3
dnisaev <dnisaev@yandex.ru>
2023-12-06 20:06:32 +0300

    add #19 lessons

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import Header from './components/Header/Header';
 import NavBar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
     return (
-        <div className={'app-wrapper'}>
-            <Header/>
-            <NavBar/>
-            <div className={'app-wrapper-content'}>
-                <Dialogs/>
-                {/*<Profile/>*/}
+        <BrowserRouter>
+            <div className={'app-wrapper'}>
+                <Header/>
+                <NavBar/>
+                <div className={'app-wrapper-content'}>
+                    <Route path={"/profile"} Component={Profile}/>
+                    <Route path={"/dialogs"} Component={Dialogs}/>
+                </div>
             </div>
-        </div>
+        // </BrowserRouter>
     );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+// const root = ReactDOM.createRoot(
+//     document.getElementById('body') as HTMLElement
+// );
+// root.render(
+//     <React.StrictMode>
+//         <App/>
+//     </React.StrictMode>
+// );
+// reportWebVitals();
+
+ReactDOM.render(<App />,  document.getElementById('root'));
