bf51df41cc6bbff4701258f89f056cd21b089ee4
dnisaev <dnisaev@yandex.ru>
2023-12-24 20:55:49 +0300

    transfer BrowserRouter to index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header/Header';
 import NavBar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import {StateType} from "./redux/state";
 
 type AppPropsType = {
@@ -13,18 +13,16 @@ type AppPropsType = {
 
 function App({state}: AppPropsType) {
     return (
-        <BrowserRouter>
-            <div className={'app-wrapper'}>
-                <Header/>
-                <NavBar/>
-                <div className={'app-wrapper-content'}>
-                    <Route path={"/profile"} render={ ()=> <Profile
-                        state={state.profilePage} />}/>
-                    <Route path={"/dialogs"} render={ ()=> <Dialogs
-                        state={state.dialogPage} />}/>
-                </div>
+        <div className={'app-wrapper'}>
+            <Header/>
+            <NavBar/>
+            <div className={'app-wrapper-content'}>
+                <Route path={"/profile"} render={() => <Profile
+                    state={state.profilePage}/>}/>
+                <Route path={"/dialogs"} render={() => <Dialogs
+                    state={state.dialogPage}/>}/>
             </div>
-        </BrowserRouter>
+        </div>
     );
 }
 
