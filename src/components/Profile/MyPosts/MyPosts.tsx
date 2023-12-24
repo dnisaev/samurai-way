938800f31dd7d709bfc4dfe7373f384a6ccb24a0
dnisaev <dnisaev@yandex.ru>
2023-12-24 22:29:36 +0300

    add createRef for onClick

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -8,16 +8,21 @@ const MyPosts = ({posts}: ProfilePageType) => {
     let postMessagesElements = posts.map((m)=>{
         return <Post key={m.id} message={m.message} likesCount={m.likesCount} id={m.id}/>
     });
+    let newPostElement = React.createRef<HTMLTextAreaElement>();
+    const addPost = () => {
+        let text = newPostElement.current?.value;
+        console.log(text);
+    }
 
     return (
         <div className={styles.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea/>
+                    <textarea ref={newPostElement}/>
                 </div>
                 <div>
-                    <button>add post</button>
+                    <button onClick={addPost}>add post</button>
                 </div>
             </div>
             <div className={styles.posts}>
